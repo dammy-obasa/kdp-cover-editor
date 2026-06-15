4a38148720f2b8807b887cbced332bb906472c2c
dammy-obasa <damilolaobasa@yahoo.com>
2026-06-14 18:43:45 -0600

    Update run_app.bat

--- run_app.bat
+++ run_app.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Launch the KDP Cover Editor web app and open it in the browser.
-cd /d "%~dp0\.."
+cd /d "%~dp0"
 start "" http://127.0.0.1:5050
-python CoverPage_sizer\app.py
+python app.py
 pause
